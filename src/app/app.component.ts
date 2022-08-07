e504a32f98cc63c19d34094927b5c66fffc5ee3c
said hasnaoui <saidhasnaoui.uce@gmail.com>
2022-08-07 21:31:28 +0100

    first commit create header, nav-list and heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {TranslateService} from "@ngx-translate/core";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'said-hasnaoui';
+  lang: String = localStorage.getItem('language') ?? 'en';
+  mode: String = localStorage.getItem('mode') ?? 'dark';
+
+  constructor(private translate: TranslateService,
+              private _formBuilder: FormBuilder,
+              private modalService: NgbModal) {
+    translate.setDefaultLang('fr');
+    translate.use('fr');
+  }
+
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+  onChangeMode(mode: string) {
+    if (mode === 'dark')
+      localStorage.setItem('mode', 'light');
+    else
+      localStorage.setItem('mode', 'dark');
+  }
+  changeLang(lang: string) {
+    localStorage.setItem('language', lang);
+    window.location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavListComponent } from './pages/sidenav-list/sidenav-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HerosComponent } from './pages/heros/heros.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {ReactiveFormsModule} from "@angular/forms";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavListComponent,
+    HeaderComponent,
+    HomeComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    HttpClientModule,
+    MatButtonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory, deps: [HttpClient]
+      }
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
